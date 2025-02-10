542dca0d8152611497bb0487671fccf2e7a21233
LangaMad <artykovh@gmail.com>
2025-02-10 19:36:38 +0600

    add urls for views & update core.urls

---
+++ test_project_beam/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, ProfileViewSet, ProductViewSet, OrderViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'profiles', ProfileViewSet)
+router.register(r'products', ProductViewSet)
+router.register(r'orders', OrderViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
+
