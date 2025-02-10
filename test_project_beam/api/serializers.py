7d79191e7d459f56e30d0efc604cdccd79ab7ab8
LangaMad <artykovh@gmail.com>
2025-02-10 19:14:11 +0600

    add serializer for model

---
+++ test_project_beam/api/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import User, Profile, Product, Order
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+
+
+
+
+
+
+
+
